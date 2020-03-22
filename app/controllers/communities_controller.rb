306c86f62aef3364bc65a4650fe76b5fac097337
hiroharu <hh.25799623@gmail.com>
2020-03-22 13:54:05 +0000

    諸々を片付けた

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -20,6 +20,31 @@ class CommunitiesController < ApplicationController
     @com = Community.find(params[:id])
   end
   
+  def add
+    unless @current_student
+      redirect_to root_path
+    end
+    com = Community.find(params[:add][:community_id])
+    stu = Student.find(params[:add][:student_id])
+    if com.auth_code.present?
+      if com.auth_code.to_s == params[:add][:auth_code]
+        com.students << stu
+        flash[:notice] = "コミュニティに参加しました。"
+        redirect_to community_path(com.id)
+      else
+        flash[:notice] = "認証コードが違います"
+        redirect_to communities_path
+      end
+    elsif com.openness
+      flash[:notice] = "自由コミュニティに参加しました。"
+      com.students << stu
+      redirect_to community_path(com.id)
+    else
+      flash[:notice] = "ダメー"
+      redirect_to communities_path
+    end
+  end
+  
   def update
     com = Community.find(params[:id])
     com.assign_attributes(image_params)
