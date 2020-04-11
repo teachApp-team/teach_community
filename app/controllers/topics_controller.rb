a324a6e7bfbab255ffe40fcaff1162758d245bcf
hiroharu <hh.25799623@gmail.com>
2020-04-11 11:52:11 +0000

    topics作成完了

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,18 @@
 class TopicsController < ApplicationController
   def index
     @results = Topic.all
+    @kawai_topics = []
+    @sundai_topics = []
+    @tousin_topics = []
+    @results.each do |topic|
+      if topic.site_name == "河合塾"
+        @kawai_topics.push(topic)
+      elsif topic.site_name == "駿台"
+        @sundai_topics.push(topic)
+      elsif topic.site_name == "東進ハイスクール"
+        @tousin_topics.push(topic)
+      end
+    end
   end
   
   def scrape_all
